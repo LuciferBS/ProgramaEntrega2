0b17898a7aff1c02dba57bc46bd3f171cd6e2496
LuciferBS <benjamin.saavedram1@gmail.com>
2019-11-16 16:35:22 -0300

    Version.0.01

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\programacion\proyectoshtml_css_javascript\entrega2\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
